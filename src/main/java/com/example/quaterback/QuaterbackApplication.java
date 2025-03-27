8aed8aabf3edc374bed135ecf73f2103058a3a18
taewon0 <ktw495@naver.com>
2025-03-27 18:38:18 +0900

    firsts commit

---
+++ src/main/java/com/example/quaterback/QuaterbackApplication.java
@@ -0,0 +1,13 @@
+package com.example.quaterback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuaterbackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuaterbackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/quaterback/QuaterbackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.quaterback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuaterbackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
