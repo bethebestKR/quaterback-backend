5fded04b26e669b75d4c66ffd2c6987f4f00450a
taewon0 <ktw495@naver.com>
2025-04-06 13:35:02 +0900

    feat : 사용자 정보 인증을 위한 CustomUserDetails 작성
    스프링 시큐리티로 사용자 정보를 인증할 수 있게
    CustomUserDetails와 CustomUserDetailsServise 추가

---
+++ src/main/java/com/example/quaterback/login/service/CustomDetailsService.java
@@ -0,0 +1,28 @@
+package com.example.quaterback.login.service;
+
+import com.example.quaterback.login.dto.CustomUserDetails;
+import com.example.quaterback.login.entity.UserEntity;
+import com.example.quaterback.login.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@RequiredArgsConstructor
+@Service
+public class CustomDetailsService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+
+        UserEntity userEntity = userRepository.findByUsername(username);
+        if (userEntity != null){
+
+            return new CustomUserDetails(userEntity);
+        }
+        return null;
+    }
+}
