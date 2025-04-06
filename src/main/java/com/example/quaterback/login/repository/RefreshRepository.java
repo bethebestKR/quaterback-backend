80ff9aee9b17a08772567e2d90501af9b5121595
taewon0 <ktw495@naver.com>
2025-04-06 13:46:17 +0900

    feat : Refresh 토큰 구현
    access토큰이 만료된 경우 401을 리턴하게 된다.
    
    이때 쿠키에 저장되어 있던 refresh token을 사용하여 재발급 받을 수 있다.
    
    보안을 위해 refresh 토큰을 db에 저장해두고 재발급 받을 때 기존 refresh는 db에서 삭제한다.

---
+++ src/main/java/com/example/quaterback/login/repository/RefreshRepository.java
@@ -0,0 +1,13 @@
+package com.example.quaterback.login.repository;
+
+import com.example.quaterback.login.entity.RefreshEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.transaction.annotation.Transactional;
+
+public interface RefreshRepository extends JpaRepository<RefreshEntity, Long> {
+
+    Boolean existsByRefresh(String refresh);
+
+    @Transactional
+    void deleteByRefresh(String refresh);
+}
