c05999068d22f4bd5d32ace64940c8e7e88f2358
taewon0 <ktw495@naver.com>
2025-04-07 14:42:02 +0900

    refactor : deleteByRefresh에서 @Transactional 제거

--- src/main/java/com/example/quaterback/login/repository/RefreshRepository.java
+++ src/main/java/com/example/quaterback/login/repository/RefreshRepository.java
@@ -2,12 +2,10 @@ package com.example.quaterback.login.repository;
 
 import com.example.quaterback.login.entity.RefreshEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.transaction.annotation.Transactional;
 
 public interface RefreshRepository extends JpaRepository<RefreshEntity, Long> {
 
     Boolean existsByRefresh(String refresh);
 
-    @Transactional
     void deleteByRefresh(String refresh);
 }
