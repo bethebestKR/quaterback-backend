16945ac98d2a78b33020537a3985ea3e7ca4ccc0
taewon0 <ktw495@naver.com>
2025-04-09 11:36:24 +0900

    refactor : 불필요한 import 제거

--- src/main/java/com/example/quaterback/login/jwt/JWTFilter.java
+++ src/main/java/com/example/quaterback/login/jwt/JWTFilter.java
@@ -13,7 +13,6 @@ import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
-import java.io.PrintWriter;
 
 @RequiredArgsConstructor
 public class JWTFilter extends OncePerRequestFilter {
