907b67bdf4bfca4a5b03f4e8fb5ef685f2cf83ab
taewon0 <ktw495@naver.com>
2025-04-09 14:18:22 +0900

    refactor : 기존 JpaRepository를 의존하는 레포의 이름 변경
    해당 레포는 SpringDataJpa에 강하게 의존하고 있다.
    그에 맞게 이름을 변경한다.

--- src/main/java/com/example/quaterback/login/repository/refresh/SpringDataRefreshRepository.java
+++ src/main/java/com/example/quaterback/login/repository/refresh/SpringDataRefreshRepository.java
@@ -1,4 +1,4 @@
-package com.example.quaterback.login.repository;
+package com.example.quaterback.login.repository.refresh;
 
 import com.example.quaterback.login.entity.RefreshEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
--- src/main/java/com/example/quaterback/login/repository/user/SpringDataJpaUserRepository.java
+++ src/main/java/com/example/quaterback/login/repository/user/SpringDataJpaUserRepository.java
@@ -3,7 +3,7 @@ package com.example.quaterback.login.repository;
 import com.example.quaterback.login.entity.UserEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface UserRepository extends JpaRepository<UserEntity, Integer> {
+public interface SpringDataJpaUserRepository extends JpaRepository<UserEntity, Integer> {
 
     Boolean existsByUsername(String username);
 
