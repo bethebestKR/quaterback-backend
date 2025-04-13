5b27e2a4420cfdb150d4289b57631a22ee5bb1cb
PlusUltraCode <kkd06144@naver.com>
2025-04-13 19:50:26 +0900

    feat(TransactionEvent) : Main.class Websocket 연결

--- src/main/java/com/example/quaterback/QuaterbackApplication.java
+++ src/main/java/com/example/quaterback/QuaterbackApplication.java
@@ -2,12 +2,13 @@ package com.example.quaterback;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.socket.config.annotation.EnableWebSocket;
 
 @SpringBootApplication
+@EnableWebSocket
 public class QuaterbackApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(QuaterbackApplication.class, args);
     }
-
 }
