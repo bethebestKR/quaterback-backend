20615ff10523506bb785a8b3433f3325e3e55426
taewon0 <ktw495@naver.com>
2025-04-20 10:31:51 +0900

    feat(BootNotification) : Converter 생성

---
+++ src/main/java/com/example/quaterback/websocket/boot/notification/converter/BootNotificationConverter.java
@@ -0,0 +1,35 @@
+package com.example.quaterback.websocket.boot.notification.converter;
+
+
+import com.example.quaterback.annotation.Converter;
+import com.example.quaterback.websocket.MessageUtil;
+import com.example.quaterback.websocket.boot.notification.domain.BootNotificationDomain;
+import com.example.quaterback.websocket.boot.notification.domain.sub.CustomData;
+import com.example.quaterback.websocket.boot.notification.domain.sub.Location;
+import com.fasterxml.jackson.databind.JsonNode;
+
+@Converter
+public class BootNotificationConverter {
+    public BootNotificationDomain convertToBootNotificationDomain(JsonNode jsonNode) {
+        String messageId = MessageUtil.getMessageId(jsonNode);
+        String messageTypeId = MessageUtil.getMessageTypeId(jsonNode);
+        String action = MessageUtil.getAction(jsonNode);
+        JsonNode payload = MessageUtil.getPayload(jsonNode);
+
+        return BootNotificationDomain.builder()
+                .messageId(messageId)
+                .messageTypeId(messageTypeId)
+                .action(action)
+                .reason(payload.path("reason").asText())
+                .model(payload.path("chargingStation").path("model").asText())
+                .customData(new CustomData(
+                        payload.path("customData").path("vendorId").asText(),
+                        payload.path("customData").path("stationId").asText(),
+                        new Location(
+                                payload.path("customData").path("location").path("latitude").asDouble(),
+                                payload.path("customData").path("location").path("longitude").asDouble(),
+                                payload.path("customData").path("location").path("address").asText())
+                ))
+                .build();
+    }
+}
