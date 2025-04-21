e44ed7d5bc8aeace79a6af98b1c156135f4ab20d
taewon0 <ktw495@naver.com>
2025-04-21 23:03:20 +0900

    refactor(BootNotification) : CustomData 이름 변경
    다른 메시지도 CustomData를 가지기 때문에 이름을 변경한다.

--- src/main/java/com/example/quaterback/websocket/boot/notification/domain/sub/BootCustomData.java
+++ src/main/java/com/example/quaterback/websocket/boot/notification/domain/sub/BootCustomData.java
@@ -7,7 +7,7 @@ import lombok.*;
 @NoArgsConstructor
 @AllArgsConstructor
 @ToString
-public class CustomData {
+public class BootCustomData {
     private String vendorId;
     private String stationId;
     private Location location;
