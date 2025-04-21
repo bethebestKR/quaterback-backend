15a5be3454f52fb04fb9c9a2fcc074194fddacc8
taewon0 <ktw495@naver.com>
2025-04-21 18:41:28 +0900

    refactor(BootNotification) : 서비스에 비즈니스 로직 추가

--- src/main/java/com/example/quaterback/websocket/boot/notification/service/BootNotificationService.java
+++ src/main/java/com/example/quaterback/websocket/boot/notification/service/BootNotificationService.java
@@ -1,11 +1,15 @@
 package com.example.quaterback.websocket.boot.notification.service;
 
+import com.example.quaterback.station.entity.ChargingStationEntity;
 import com.example.quaterback.station.repository.ChargingStationRepository;
 import com.example.quaterback.websocket.boot.notification.converter.BootNotificationConverter;
 import com.example.quaterback.websocket.boot.notification.domain.BootNotificationDomain;
 import com.fasterxml.jackson.databind.JsonNode;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.time.LocalDateTime;
 
 @Service
 @RequiredArgsConstructor
@@ -14,10 +18,16 @@ public class BootNotificationService {
     private final ChargingStationRepository chargingStationRepository;
     private final BootNotificationConverter converter;
 
-
+    @Transactional
     public String stationPowerUp(JsonNode jsonNode) {
         BootNotificationDomain domain = converter.convertToBootNotificationDomain(jsonNode);
-        String stationId = chargingStationRepository.updateStationStatus(domain.extractStationId());
-        return stationId;
+        ChargingStationEntity entity = chargingStationRepository.findByStationId(domain.extractStationId());
+
+        if (entity.getStationStatus().equals("inactive")) {
+            entity.setStationStatus("active");
+            entity.setUpdateStatusTimeStamp(LocalDateTime.now());
+        }
+
+        return entity.getStationId();
     }
 }
