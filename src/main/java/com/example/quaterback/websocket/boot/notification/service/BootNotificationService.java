0fa15420e69d7e03f639abedff9b41ea977076ad
taewon0 <ktw495@naver.com>
2025-05-02 10:42:49 +0900

    refactor(BootNotification) : Boot 서비스, 핸들러 수정
    -핸들러에서 서비스에 세션 id를 넘겨준다.
    -서비스에서 redis service를 통해 stationId를 알아낸 후 사용한다.

--- src/main/java/com/example/quaterback/websocket/boot/notification/service/BootNotificationService.java
+++ src/main/java/com/example/quaterback/websocket/boot/notification/service/BootNotificationService.java
@@ -3,6 +3,7 @@ package com.example.quaterback.websocket.boot.notification.service;
 import com.example.quaterback.api.domain.station.constant.StationStatus;
 import com.example.quaterback.api.domain.station.domain.ChargingStationDomain;
 import com.example.quaterback.api.domain.station.repository.ChargingStationRepository;
+import com.example.quaterback.redis.service.RedisMapSessionToStationService;
 import com.example.quaterback.websocket.boot.notification.converter.BootNotificationConverter;
 import com.example.quaterback.websocket.boot.notification.domain.BootNotificationDomain;
 import com.fasterxml.jackson.databind.JsonNode;
@@ -16,16 +17,18 @@ public class BootNotificationService {
 
     private final ChargingStationRepository chargingStationRepository;
     private final BootNotificationConverter converter;
+    private final RedisMapSessionToStationService redisMappingService;
 
     @Transactional
-    public String updateStationStatus(JsonNode jsonNode) {
+    public String updateStationStatus(JsonNode jsonNode, String sessionId) {
         BootNotificationDomain bootNotificationDomain = converter.convertToBootNotificationDomain(jsonNode);
 
-        ChargingStationDomain chargingStationDomain = chargingStationRepository.findByStationId(bootNotificationDomain.extractStationId());
+        String stationId = redisMappingService.mapSessionToStation(sessionId, bootNotificationDomain.extractStationId());
+        ChargingStationDomain chargingStationDomain = chargingStationRepository.findByStationId(stationId);
 
         chargingStationDomain.updateStationStatus(StationStatus.ACTIVE);
-        String stationId = chargingStationRepository.update(chargingStationDomain);
+        String resultStationId = chargingStationRepository.update(chargingStationDomain);
 
-        return stationId;
+        return resultStationId;
     }
 }
