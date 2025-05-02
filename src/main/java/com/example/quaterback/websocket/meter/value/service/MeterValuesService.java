5058bba6b596caf3c93e40fa943adacf1527808c
taewon0 <ktw495@naver.com>
2025-05-02 11:39:39 +0900

    chore(MeterValuesService) : 주석 제거

--- src/main/java/com/example/quaterback/websocket/meter/value/service/MeterValuesService.java
+++ src/main/java/com/example/quaterback/websocket/meter/value/service/MeterValuesService.java
@@ -26,17 +26,12 @@ public class MeterValuesService {
         SampledValue sampledValue = meterValue.getSampledValues().get(0);
         Integer value = sampledValue.getValue();
 
-
-        //Redis를 이용해 인메모리에 있는 sessionId 의 value 값으로 되어있는 stationId 추출
         String stationId = redisService.getStationId(sessionId);
 
-        //해당 아이디를 이용해 해당하는 충전소 Entity 객체 하나를 뽑아오면서 동시에 domain 으로 변환한 걸 반환
         ChargingStationDomain chargingStationDomain = chargingStationRepository.findByStationId(stationId);
 
-        //뽑아온 도메인을 수정하고
         chargingStationDomain.updateStationEssValue(value);
 
-        //수정된 도메인을 통해 Entity를 수정한다.
         stationId = chargingStationRepository.updateEss(chargingStationDomain);
         return stationId;
     }
