f39b70b9e0f301d58f7553910f7b4f8208fcb047
taewon0 <ktw495@naver.com>
2025-05-04 11:34:01 +0900

    chore(data.sql) : ess_value도 초기값 넣어주기

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,4 +1,4 @@
-INSERT INTO charging_station (station_id, model, vendor_id, latitude, longitude, address, update_status_time_stamp, station_status) VALUES ("station-001", "R1", "quarterback", 37.5665, 126.9780, "서울특별시 중구 세종대로 110", "2025-04-17T11:20:00", "inactive");
+INSERT INTO charging_station (station_id, model, vendor_id, latitude, longitude, address, update_status_time_stamp, station_status, ess_value) VALUES ("station-001", "R1", "quarterback", 37.5665, 126.9780, "서울특별시 중구 세종대로 110", "2025-04-17T11:20:00", "inactive", 40000);
 
 INSERT INTO charger_info (evse_id, charger_status, update_status_time_stamp, station_id) VALUES (1, "Available", "2025-04-20T12:30:00", "station-001");
 INSERT INTO charger_info (evse_id, charger_status, update_status_time_stamp, station_id) VALUES (2, "Available", "2025-04-20T12:30:00", "station-001");
