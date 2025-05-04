df138faac3ba29478373ab94c96189f2cee6a214
dbspq52 <139411660+dbspq52@users.noreply.github.com>
2025-05-04 11:20:06 +0900

    refactor(RedisConfig)

--- src/main/java/com/example/quaterback/common/redis/RedisConfig.java
+++ src/main/java/com/example/quaterback/common/redis/RedisConfig.java
@@ -2,25 +2,33 @@ package com.example.quaterback.common.redis;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
-import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.GenericToStringSerializer;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 @Configuration
 public class RedisConfig {
     @Bean
-    public RedisTemplate<String, Object> redisTemplate(RedisConnectionFactory factory) {
-        RedisTemplate<String, Object> template = new RedisTemplate<>();
+    @Primary
+    public RedisTemplate<String, String> redisTemplate(RedisConnectionFactory factory) {
+        RedisTemplate<String, String> template = new RedisTemplate<>();
         template.setConnectionFactory(factory);
 
-        // 키와 값을 문자열 및 JSON으로 직렬화
         template.setKeySerializer(new StringRedisSerializer());
-        template.setValueSerializer(new Jackson2JsonRedisSerializer<>(Object.class));
-
-        template.setHashKeySerializer(new StringRedisSerializer());
-        template.setHashValueSerializer(new Jackson2JsonRedisSerializer<>(Object.class));
+        template.setValueSerializer(new StringRedisSerializer());
 
         return template;
     }
+
+       @Bean
+        public RedisTemplate<String, Long> longRedisTemplate(RedisConnectionFactory factory) {
+            RedisTemplate<String, Long> template = new RedisTemplate<>();
+            template.setConnectionFactory(factory);
+            template.setKeySerializer(new StringRedisSerializer());
+            template.setValueSerializer(new GenericToStringSerializer<>(Long.class)); // Long을 String처럼 저장
+            return template;
+        }
 }
+
