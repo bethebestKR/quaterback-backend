00c589dda243b8b471c7fcaac9f54d25e73e00fc
taewon0 <ktw495@naver.com>
2025-05-06 18:14:57 +0900

    chore(Data) : 데이터 초기화 파일 수정

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,8 +1,18 @@
-INSERT INTO charging_station (station_id, model, vendor_id, latitude, longitude, address, update_status_time_stamp, station_status, ess_value) VALUES ("station-001", "R1", "quarterback", 37.5665, 126.9780, "서울특별시 중구 세종대로 110", "2025-04-17T11:20:00", "inactive", 40000);
+-- 먼저 지우고 다시 넣기
+DELETE FROM charger_info WHERE station_id = 'station-001';
+DELETE FROM charging_station WHERE station_id = 'station-001';
 
-INSERT INTO charger_info (evse_id, charger_status, update_status_time_stamp, station_id) VALUES (1, "Available", "2025-04-20T12:30:00", "station-001");
-INSERT INTO charger_info (evse_id, charger_status, update_status_time_stamp, station_id) VALUES (2, "Available", "2025-04-20T12:30:00", "station-001");
-INSERT INTO charger_info (evse_id, charger_status, update_status_time_stamp, station_id) VALUES (3, "Available", "2025-04-20T12:30:00", "station-001");
+-- 그 다음 새로 삽입
+INSERT INTO charging_station (station_id, model, vendor_id, latitude, longitude, address, update_status_time_stamp, station_status)
+VALUES ('station-001', 'R1', 'quarterback', 37.5665, 126.9780, '서울특별시 중구 세종대로 110', '2025-04-17T11:20:00', 'inactive');
+
+INSERT INTO charger_info (evse_id, charger_status, update_status_time_stamp, station_id)
+VALUES (1, 'Available', '2025-04-20T12:30:00', 'station-001');
+INSERT INTO charger_info (evse_id, charger_status, update_status_time_stamp, station_id)
+VALUES (2, 'Available', '2025-04-20T12:30:00', 'station-001');
+INSERT INTO charger_info (evse_id, charger_status, update_status_time_stamp, station_id)
+VALUES (3, 'Available', '2025-04-20T12:30:00', 'station-001');
+
 
 INSERT INTO customer (customer_id, customer_name, id_token, email, phone, vehicle_no, registration_date) VALUES ("user1", "이름1", "token001", "e1@gmail.com", "01012345678", "12-4234", "2024-05-20T16:30:00");
 INSERT INTO customer (customer_id, customer_name, id_token, email, phone, vehicle_no, registration_date) VALUES ("user2", "이름2", "user-002", "e2@gmail.com", "01034550101", "12-2353", "2024-05-21T16:30:00");
