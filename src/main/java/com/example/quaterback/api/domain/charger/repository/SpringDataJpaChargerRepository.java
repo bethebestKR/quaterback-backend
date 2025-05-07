6287bf02c81553a508c05a4d026aea753994ffbf
dbspq52 <139411660+dbspq52@users.noreply.github.com>
2025-05-07 11:17:04 +0900

    feature: (SpringDataJpaChargerRepository):station에 해당하는 모든 충전기 반환

--- src/main/java/com/example/quaterback/api/domain/charger/repository/SpringDataJpaChargerRepository.java
+++ src/main/java/com/example/quaterback/api/domain/charger/repository/SpringDataJpaChargerRepository.java
@@ -3,8 +3,10 @@ package com.example.quaterback.api.domain.charger.repository;
 import com.example.quaterback.api.domain.charger.entity.ChargerEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface SpringDataJpaChargerRepository extends JpaRepository<ChargerEntity, Long> {
     Optional<ChargerEntity> findByStation_StationIdAndEvseId(String stationId, Integer evseId);
+    List<ChargerEntity> findByStation_StationId(String stationId);
 }
