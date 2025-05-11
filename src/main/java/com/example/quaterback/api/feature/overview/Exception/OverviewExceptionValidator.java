3a2a714b8104e31f51fcf974c1835d354b62c9e2
dbspq52 <139411660+dbspq52@users.noreply.github.com>
2025-05-11 14:11:51 +0900

    feature(OverviewExceptionValidator)
    stationId 형식 갖추기

---
+++ src/main/java/com/example/quaterback/api/feature/overview/Exception/OverviewExceptionValidator.java
@@ -0,0 +1,17 @@
+package com.example.quaterback.api.feature.overview.Exception;
+
+import org.apache.coyote.BadRequestException;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.util.regex.Pattern;
+
+public class OverviewExceptionValidator {
+    private static final Pattern STATION_ID_PATTERN = Pattern.compile("^station-\\d+$");
+
+    public static void validateStationId(String stationId){
+        if(stationId == null || !STATION_ID_PATTERN.matcher(stationId).matches()){
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "stationId는 'station-숫자' 형식이어야 합니다.");
+        }
+    }
+}
