8e0fc9f9312e5f845e8107244eac87d323cc3a46
dbspq52 <139411660+dbspq52@users.noreply.github.com>
2025-05-15 19:46:35 +0900

    .

--- src/main/java/com/example/quaterback/api/feature/statistics/controller/StatisticsController.java
+++ src/main/java/com/example/quaterback/api/feature/statistics/controller/StatisticsController.java
@@ -2,8 +2,9 @@ package com.example.quaterback.api.feature.statistics.controller;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 @RequiredArgsConstructor
 public class StatisticsController {
 
