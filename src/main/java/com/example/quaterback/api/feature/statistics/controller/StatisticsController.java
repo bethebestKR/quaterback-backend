388b643e86b2974807ebd31d1f8629d40a446537
PlusUltraCode <kkd06144@naver.com>
2025-05-18 11:25:57 +0900

    feat(Modeling) : 3D Modeling api 개발 완료

--- src/main/java/com/example/quaterback/api/feature/statistics/controller/StatisticsController.java
+++ src/main/java/com/example/quaterback/api/feature/statistics/controller/StatisticsController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-@RequestMapping("/statistics")
+@RequestMapping("/api/statistics")
 @RestController
 @RequiredArgsConstructor
 public class StatisticsController {
