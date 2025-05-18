43ef1e1b0916d5755d8c57d5dd00e943c31c94b2
dbspq52 <139411660+dbspq52@users.noreply.github.com>
2025-05-18 13:13:19 +0900

    feature(KepcoRepository)

--- src/main/java/com/example/quaterback/api/domain/price/repository/KepcoRepository.java
+++ src/main/java/com/example/quaterback/api/domain/price/repository/KepcoRepository.java
@@ -5,6 +5,9 @@ import com.example.quaterback.api.domain.price.constant.TimeSlot;
 import com.example.quaterback.api.domain.price.entity.KepcoPrice;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface KepcoRepository extends JpaRepository<KepcoPrice, Long> {
     KepcoPrice findBySeasonAndTimeSlot(Season season, TimeSlot timeSlot);
+    List<KepcoPrice> findBySeason(Season season);
 }
