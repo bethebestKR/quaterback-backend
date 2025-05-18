7664f8325bf50027ff8418dbbe6737666b470390
dbspq52 <139411660+dbspq52@users.noreply.github.com>
2025-05-18 18:24:03 +0900

    feature(price_failure)

--- src/main/java/com/example/quaterback/api/domain/price/repository/PriceRepository.java
+++ src/main/java/com/example/quaterback/api/domain/price/repository/PriceRepository.java
@@ -2,9 +2,11 @@ package com.example.quaterback.api.domain.price.repository;
 
 import com.example.quaterback.api.domain.price.entity.PricePerMwh;
 import com.example.quaterback.api.feature.statistics.dto.response.StatisticsData;
+import io.lettuce.core.dynamic.annotation.Param;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 public interface PriceRepository extends JpaRepository<PricePerMwh, Long> {
@@ -23,4 +25,14 @@ public interface PriceRepository extends JpaRepository<PricePerMwh, Long> {
         ORDER BY p.updated_date_time DESC
         """, nativeQuery = true)
     List<PricePerMwh> findDailyCsPrice7DayRaw();
+
+    @Query("""
+        SELECT p FROM PricePerMwh p
+        WHERE p.updatedDateTime BETWEEN :start AND :end
+        ORDER BY p.updatedDateTime ASC
+    """)
+    List<PricePerMwh> findByUpdatedDateTimeBetween(
+            @Param("start") LocalDateTime start,
+            @Param("end") LocalDateTime end
+    );
 }
